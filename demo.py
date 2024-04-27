b2f7a8e572d940ff15fa8522f9835deb6424462c
23668 <2366815033@qq.com>
2024-04-27 15:01:10 +0800

    更改

--- demo.py
+++ demo.py
@@ -1,8 +1,5 @@
 import numpy as np
 
-z = np.ones((4, 4), dtype=int)
-z[:, 2] = 2
-print(z)
-z = np.matrix(z)
-print(z*z)
-print(z*z.T)
+x = np.array([1,2,3,4,5,6,7,8,9,10])
+x = np.matrix(x)
+print(x.T*x)
