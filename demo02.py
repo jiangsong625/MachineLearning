5a3771fd916c6a7e7fb658941492a2db0d4f7b36
23668 <2366815033@qq.com>
2024-05-02 14:19:50 +0800

    更改

--- demo02.py
+++ demo02.py
@@ -1,18 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LogisticRegression
+import pandas as pd
 
 
-X = np.array([[0.5, 1.5], [1,1], [1.5, 0.5], [3, 0.5], [2, 2], [1, 2.5]])
-y = np.array([0, 0, 0, 1, 1, 1])
-
-print(y)
+df = pd.read_csv(filepath_or_buffer='dataFile/ex2data1.txt', names=['text1', 'text2', 'lable'])
+X = df.iloc[:, 0:2]
+y = df.iloc[:, 2]
+X = np.array(X)
+y = np.array(y).reshape(y.shape[0],)
 lr_model = LogisticRegression()
 lr_model.fit(X, y)
-print(lr_model.intercept_, lr_model.coef_)
-fig, ax = plt.subplots(1, 1, figsize=(5, 5))
-ax.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap='autumn')
-x0 = np.arange(0, 3, 0.1)
-x1 = (-lr_model.intercept_ - x0 * lr_model.coef_[0][0]) / lr_model.coef_[0][1]
-ax.plot(x0, x1, 'k', lw=1)
+print(lr_model.coef_)
+x0 = np.linspace(df['text1'].min(), df['text1'].max(), 100)
+x1 = (-1 / lr_model.coef_[0][1]) * (lr_model.coef_[0][0] * x0 + lr_model.intercept_)
+# 打印图像
+fig, ax = plt.subplots(figsize=(12, 8))
+ax.scatter(df['text1'], df['text2'], c=df['lable'], s=40, cmap=plt.cm.Spectral)
+ax.set_xlabel('Exam 1 score')
+ax.set_ylabel('Exam 2 score')
+ax.plot(x0, x1, color='red', linewidth=3)
 plt.show()
--- logisticRegression.py
+++ logisticRegression.py
@@ -16,7 +16,7 @@ def load_and_preprocess_data():
     X = df.iloc[:, 0:2]
     y = df.iloc[:, 2]
     # X = feature_scaling(X)
-    # X.insert(0, 'ones', 1)
+    X.insert(0, 'ones', 1)
     X = np.matrix(X)
     y = np.matrix(y).T
     theta = np.matrix(np.zeros(X.shape[1]))
@@ -63,20 +63,13 @@ def visualize_cost(cost, iters):
 # 主程序
 if __name__ == '__main__':
     X, y, df, theta = load_and_preprocess_data()
-    X = np.array(X)
-    y = np.array(y).reshape(y.shape[0],)
-    lr_model = LogisticRegression()
-    lr_model.fit(X, y)
-    print(lr_model.coef_)
-    x0 = np.linspace(df['text1'].min(), df['text1'].max(), 100)
-    x1 = (-1 / lr_model.coef_[0][1]) * (lr_model.coef_[0][0] * x0 + lr_model.intercept_)
-    # 打印图像
-    fig, ax = plt.subplots(figsize=(12, 8))
-    ax.scatter(df['text1'], df['text2'], c=df['lable'], s=40, cmap=plt.cm.Spectral)
-    ax.set_xlabel('Exam 1 score')
-    ax.set_ylabel('Exam 2 score')
-    ax.plot(x0, x1, color='red', linewidth=3)
-    plt.show()
+    print(compute_cost(X, y, theta))
+    m = X.shape[0]
+    alpha = 0.01
+    error = sigmoid(np.dot(X, theta.T)) - y
+    theta = theta - np.dot(error.T, X) * (alpha / m)
+    print(theta)
+    print(compute_cost(X, y, theta))
 
 
 
