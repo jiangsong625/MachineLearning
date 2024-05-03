accdeb65de250971575a6aea9b9f2e2e03a26366
23668 <2366815033@qq.com>
2024-05-03 14:29:10 +0800

    更改

--- logisticRegression.py
+++ logisticRegression.py
@@ -11,10 +11,9 @@ def feature_scaling(X):
 
 # 加载数据
 def load_and_preprocess_data():
-    df = pd.read_csv(filepath_or_buffer='dataFile/ex2data1.txt', names=['text1', 'text2', 'lable'])
+    df = pd.read_csv(filepath_or_buffer='dataFile/ex2data1.txt', names=['text1', 'text2', 'label'])
     X = df.iloc[:, 0:2]
     y = df.iloc[:, 2]
-    X = feature_scaling(X)
     X = np.matrix(X)
     y = np.matrix(y).T
     theta = np.matrix(np.zeros(X.shape[1]))
@@ -28,34 +27,36 @@ def sigmoid(z):
 
 
 # 计算代价函数
-def compute_cost(X, y, theta, b):
+def compute_cost(X, y, w, b):
     m = X.shape[0]
-    h = sigmoid(np.dot(X, theta.T) + b)
+    h = sigmoid(np.dot(X, w) + b)
     J = (1 / m) * (-np.log(h).T.dot(y) - np.log(1 - h).T.dot(1 - y))
     return J[0, 0]
 
+
 # 计算梯度
-def compute_gradient_logistic(X, y, theta, b):
+def compute_gradient_logistic(X, y, w, b, lambda_=None):
     m = X.shape[0]
-    h = sigmoid(np.dot(X, theta.T) + b)
-    error = h - y
-    grad = (1 / m) * (error.T.dot(X))
-    b = (1 / m) * np.sum(error)
-    return grad, b
+    h = sigmoid(np.dot(X, w) + b)
+    dj_dw = (1 / m) * X.T.dot(h - y)
+    dj_db = (1 / m) * np.sum(h - y)
+    return dj_dw, dj_db
 
 
 # 梯度下降算法
-def gradient_descent(theta, b, X, y, alpha, iters):
-    m = X.shape[0]
-    cost = np.zeros(iters)
+def gradient_descent(X, y, w_in, b_in, const_function, gradient_function, alpha, iters, lambda_):
+    J_history = []
+    w_history = []
     for i in range(iters):
-        temp_theta, temp_b = compute_gradient_logistic(X, y, theta, b)
-        theta -= temp_theta * alpha
-        b -= temp_b * alpha
-        cost[i] = compute_cost(X, y, theta, b)
-        if i % 100 == 0:
-            print(f'迭代次数：{i}代价：{cost[i]}')
-    return theta, cost, b
+        dj_dw, dj_db = gradient_function(X, y, w_in, b_in, lambda_)
+        w_in = w_in - alpha * dj_dw
+        b_in = b_in - alpha * dj_db
+        J_history.append(const_function(X, y, w_in, b_in))
+        if i % 1000 == 0:
+            w_history.append(w_in)
+            print(f'Iteration {i:4d}: Cost {J_history[-1]:8.2f}')
+
+    return w_in, b_in, J_history, w_history
 
 
 # 代价可视化
@@ -71,23 +72,39 @@ def visualize_cost(cost, iters):
     plt.show()
 
 
+# 画出决策边界
+def visualize_boundary(df, model_predictions, title):
+    fig, ax = plt.subplots(figsize=(12, 8))
+    positive = df[df['label']==1]
+    negative = df[df['label']==0]
+    ax.scatter(positive['text1'], positive['text2'], marker='x', c='r', label='Admitted')
+    ax.scatter(negative['text1'], negative['text2'], marker='o', c='b', label='Not Admitted')
+    ax.plot(df['text1'], model_predictions, 'b-')
+    ax.set_title(title)
+    plt.show()
+
+
 # 主程序
 if __name__ == '__main__':
-    X, y, df, theta, b = load_and_preprocess_data()
-    print(compute_cost(X, y, theta, b))
-    theta, b = compute_gradient_logistic(X, y, theta, b)
-    theta, cost, b = gradient_descent(theta, b, X, y, 1, 1500)
-    print(cost[-1])
-    result = X.dot(theta.T) + b
-    count = 0
-    for i in range(len(result)):
-        if result[i] > 0.5:
-            result[i] = 1
-        else:
-            result[i] = 0
-        if result[i] == y[i]:
-            count += 1
-    print(count/len(result))
+    X, y, df, w, b = load_and_preprocess_data()
+    iteration = 10000
+    alpha = 0.001
+    initial_w = 0.01 * (np.random.rand(2).reshape(-1, 1) - 0.5)
+    initial_b = -8
+    print(X.dot(initial_w))
+    theta, b, J_history, w_history = gradient_descent(
+        X, y, initial_w, initial_b, compute_cost,
+        compute_gradient_logistic,
+        alpha, iteration, 0)
+    # visualize_cost(J_history, 10000)
+    print(theta, b)
+    w1, w2 = theta[0, 0], theta[1, 0]
+    model_predictions = -(w1*X[:, 0] + b) / w2
+    print(model_predictions)
+    visualize_boundary(df, model_predictions, 'Logistic Regression')
+
+
+
 
 
 
