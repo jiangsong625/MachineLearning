3c5509f06b706359f0a05a9789902b9d3f6fa0be
23668 <2366815033@qq.com>
2024-06-09 10:57:39 +0800

    更改

--- AdvancedLearningAlgorithms/demo01.py
+++ AdvancedLearningAlgorithms/demo01.py
@@ -1,19 +1,9 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import tensorflow as tf
-from tensorflow.keras.layers import Dense, Input
-from tensorflow.keras import Sequential
-from tensorflow.keras.losses import MeanSquaredError, BinaryCrossentropy
-from tensorflow.keras.activations import sigmoid
-import logging
+import tensorflow_datasets as tfds
 
 
-logging.getLogger("tensorflow").setLevel(logging.ERROR)  # 暂时取消警告
-
-
-X_train = np.array([[1.], [2.]], dtype=np.float32)
-Y_train = np.array([[300.], [500.]], dtype=np.float32)
-linear_model = Dense(units=1, activation='linear', name='linear_model')
-print(linear_model.weights)
-
+builder = tfds.list_builders()
+print(builder)
+data, info = tfds.load("mnist", with_info=True)
+train_data, test_data = data['train'], data['test']
+print(test_data)
 
