c8769b1b822d3edce18560ab546a859601a4e09d
23668 <2366815033@qq.com>
2024-06-12 10:17:18 +0800

    更改

--- AdvancedLearningAlgorithms/test01.py
+++ AdvancedLearningAlgorithms/test01.py
@@ -1,18 +1,3 @@
-import scipy.io as sio
-import numpy as np
-import scipy.optimize as opt
-import matplotlib
-import matplotlib.pyplot as plt
 import tensorflow as tf
-from tensorflow.keras.layers import Dense
-from tensorflow.keras.models import Sequential
-
-
-tf.random.set_seed(1234)
-model = Sequential([
-    Dense(units=120, activation='Relu'),
-    Dense(units=40,  activation='Relu'),
-    Dense(units=6,   activation='linear')
-])
-model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.01),
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True))
+print(tf.test.is_gpu_avaiable())
+print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
